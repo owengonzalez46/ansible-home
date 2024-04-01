f8d6c0cd036e28de32c91b1eb4f4b3a9f6c060a0
Carlos Owen Gonzalez <owengonzalez46@gmail.com>
2024-04-01 16:29:57 -0500

    Update upd-apt.bash

--- update/upd-apt.bash
+++ update/upd-apt.bash
@@ -1,17 +1,23 @@
-#!/bin/bash
+- hosts: all
+  tasks:
+    - name: Execute Bash script to update and upgrade apt packages
+      become: yes
+      ansible.builtin.shell: |
+        #!/bin/bash
+        # Define all hosts (space-separated list)
+        all_hosts="192.168.5.6 192.168.1.6"
 
-# Define all hosts (space-separated list)
-all_hosts="192.168.5.6 192.168.1.6"
+        # Function to update and upgrade packages using sudo
+        update_and_upgrade() {
+          host=$1
 
-# Function to update and upgrade packages using sudo
-update_and_upgrade() {
-  host=$1
+          echo "Updating and upgrading packages on $host with sudo..."
+          ssh "$host" "sudo apt-get update && sudo apt-get -y upgrade"
+        }
 
-  echo "Updating and upgrading packages on $host with sudo..."
-  ssh "$host" "sudo apt-get update && sudo apt-get -y upgrade"
-}
-
-# Call the function for each host
-for host in $all_hosts; do
-  update_and_upgrade "$host"
-done
+        # Call the function for each host
+        for host in $all_hosts; do
+          update_and_upgrade "$host"
+        done
+      args:
+        executable: /bin/bash
